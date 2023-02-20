0fa934f491a7c3033e08b63d5733dbf0dcd36ce5
Julio Cesar <julioreis044@gmail.com>
2023-02-20 14:21:22 -0300

    Criando rotas da tela do administrador

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const path = require('path');
 const { home } = require('./controllers/HomeController');
 const homeRouter = require('./routes/homeRouter')
 const UsersRouter = require('./routes/usersRouter')
+const AdminRouter = require('./routes/adminRouter');
 
 
 const app = express();
@@ -17,6 +18,7 @@ const port = 3000;
 app.use(express.json());
 app.use(homeRouter);
 app.use(UsersRouter);
+app.use(AdminRouter);
 
 
 app.set('view engine', 'ejs');
@@ -27,12 +29,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'))
 app.use(express.static(path.resolve("src", "public")));
 
-
-
-
 app.get('/', (req, res) =>{
     res.render("home.ejs");
 })
-
 
 app.listen(port, () => console.log (`Servidor funcionando na porta ${port}`))
