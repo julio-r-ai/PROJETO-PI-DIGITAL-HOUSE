67e6e6fdf96f6e72721f512d8e31d24fb6caa119
Julio Cesar <julioreis044@gmail.com>
2023-03-06 21:42:53 -0300

    Trantando upload de arquivos

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -9,26 +9,22 @@ router.get('/admin/produtos', AdminController.showProdutos);
 router.get('/admin/cadastrarProduto', AdminController.showCadastroProduto);
 router.get('/admin/produtos/:id/editar', AdminController.showEditarProduto);
 
-router.post('/admin/login', AdminController.login)
-
-//const upload = multer({dest: 'src/public/img/teste'})
+/* router.post('/admin/login', AdminController.login); */
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback)=>{
-    callback(null, '/img/teste');
+    destination: function(req, file, callback){
+        callback(null, 'src/public/img')
     },
-    filename: (re, file, callback)=>{
-        let persistedFilename = `${Date.now()}_product_${file.originalname}`;
-        callback(null, persistedFilename);
+    filename: function(req, file, callback){
+        let persistedFilename = `${Date.now()}_product_${file.originalname}`
+        callback(null, persistedFilename)
     }
-
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 router.post('/admin/produtos/cadastro', upload.single('image'), AdminController.cadastroProduto);
 router.put('/admin/produtos/:id/editar', AdminController.updateProduto);
 router.delete('/admin/produtos/:id/deletar', AdminController.deleteProduto);
-
 
 module.exports = router;        
