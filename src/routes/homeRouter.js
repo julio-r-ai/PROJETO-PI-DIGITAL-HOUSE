aa9776acd068492075f187dbaf47ebce8e90da7f
Julio Cesar <julioreis044@gmail.com>
2023-03-15 00:40:08 -0300

    Arrumando botao da tela login

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 
 router.get('/', HomeController.home);
-router.get('/home', HomeController.home);
 router.get('/bolos', HomeController.bolos);
 router.get('/contato', HomeController.contato);
 router.get('/sobre', HomeController.sobre);
